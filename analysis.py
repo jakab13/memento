1ccc8c035065308538062f37ce9db90d80080109
pfriedrich-hub <lukas.lange01@gmail.com>
2023-12-05 16:40:49 +0100

    Print linear regression results

--- analysis.py
+++ analysis.py
@@ -5,13 +5,15 @@ import os
 from config import COLOURS, NUMBERS
 import seaborn as sns
 import matplotlib.pyplot as plt
+from scipy.stats import linregress
 
 pd.options.mode.chained_assignment = None
-
+sns.set_theme()
 results_folder = pathlib.Path(os.getcwd()) / "Results"
 
+
 def load_df():
-    subjects_excl = ["test"]
+    subjects_excl = ["test", "holubowska", "jakab", "gina"]
 
     subjects = [s for s in os.listdir(results_folder) if not s.startswith('.')]
     subjects = sorted([s for s in subjects if not any(s in excl for excl in subjects_excl)])
@@ -39,12 +41,12 @@ def load_df():
             masker_params = slab.ResultsFile.read_file(path, tag="masker_params")
             response_params = slab.ResultsFile.read_file(path, tag="response_params")
 
-            df_curr = pd.DataFrame()
-            df_curr["subject_id"] = [exp_params["subject_id"]] * block_length
-            df_curr["task_type"] = [task_params["type"]] * block_length
-            df_curr["task_phase"] = [task_params["phase"]] * block_length
-            df_curr["task_plane"] = [task_params["plane"]] * block_length
-            df_curr["task_level"] = [task_params["level"]] * block_length
+            df_curr = pd.DataFrame(index=range(0, block_length))
+            df_curr["subject_id"] = exp_params["subject_id"]
+            df_curr["task_type"] = task_params["type"]
+            df_curr["task_phase"] = task_params["phase"]
+            df_curr["task_plane"] = task_params["plane"]
+            df_curr["task_level"] = task_params["level"]
             df_curr["trial_timestamp"] = [trial["timestamp"] for trial in trial_params]
             df_curr["trial_index"] = [trial["index"] for trial in trial_params]
             df_curr["target_talker_id"] = [target["talker_id"] for target in target_params]
@@ -67,12 +69,22 @@ def load_df():
             df_curr["response_number"] = [response["number"] for response in response_params]
             df_curr["response_timestamp"] = [response["timestamp"] for response in response_params]
             df = pd.concat([df, df_curr], ignore_index=True)
-
+    df["masker_segment_length"] = df["masker_segment_length"].astype(float)
+    df["target_segment_length"] = df["target_segment_length"].astype(float)
     df["score_colour_correct"] = df["target_colour"] == df["response_colour"]
     df["score_number_correct"] = df["target_number"] == df["response_number"]
     df["score"] = (df["score_colour_correct"] * len(COLOURS) + df["score_number_correct"] * len(NUMBERS)) / (len(COLOURS) + len(NUMBERS))
-    df["score"] = df["score"].astype('float64')
+    df["score"] = df["score"].astype(float)
     return df
+
+
+def print_current_score(subject_id=None):
+    df = load_df()
+    df = df[df["subject_id"] == subject_id] if subject_id is not None else df
+    df = df[df["task_phase"] == "experiment"]
+    mean = df["score"].mean()
+    if not pd.isna(mean):
+        print(subject_id, "current average is:", f"{round(mean, 2) * 100}%")
 
 
 def plot_results(subject_id=None, task_type="single_source"):
@@ -87,7 +99,16 @@ def plot_results(subject_id=None, task_type="single_source"):
             y="score",
             scatter=False
         )
+        title = subject_id or "all subjects"
+        ax.set_title(f"Temporal unmasking ({title})")
+        ax.set(xlabel='Segment length (ms)', ylabel='Intelligibility (%)')
+        linreg = linregress(x=df["target_segment_length"], y=df["score"])
+        print(f"Linreg p-value: {subject_id}:  \t{round(linreg.pvalue, 4)}")
     elif task_type == "multi_source":
+        for plane in df["task_plane"].unique():
+            df_plane = df[df["task_plane"] == plane]
+            linreg = linregress(x=df_plane["masker_segment_length"], y=df_plane["score"])
+            print(f"Linreg p-value: {subject_id}/{plane}:  \t{round(linreg.pvalue, 4)}")
         ax = sns.pointplot(
             data=df,
             x="masker_segment_length",
@@ -102,3 +123,14 @@ def plot_results(subject_id=None, task_type="single_source"):
         ax.set(xlabel='Masker segment length (ms)', ylabel='Target intelligibility (%)')
     ax.set_ylim(0, 1.1)
     plt.show()
+
+
+def print_multi_source_linear_model(subject_id=None):
+    df = load_df()
+    df = df[df["task_phase"] == "experiment"]
+    df = df[df["task_type"] == "multi_source"]
+    df = df[df["subject_id"] == subject_id] if subject_id is not None else df
+    for plane in df["task_plane"].unique():
+        df_plane = df[df["task_plane"] == plane]
+        linreg = linregress(x=df_plane["masker_segment_length"], y=df_plane["score"])
+        print(f"Linreg p-value: {subject_id}/{plane}:  \t{round(linreg.pvalue, 4)}")
