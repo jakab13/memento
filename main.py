4cd037383242f464030f8ee578bc26d42679792b
pfriedrich-hub <lukas.lange01@gmail.com>
2023-12-05 16:42:13 +0100

    Excluded reversed intro

--- main.py
+++ main.py
@@ -1,33 +1,30 @@
+# TODO block button press until sound is being played
+# TODO save settings as slab config files
 
 from experiment import *
-from analysis import plot_results
+from analysis import plot_results, print_multi_source_linear_model
 
-subject_id = "jakab"
+subject_id = "pilot_varvara"
 
 exp = Experiment(subject_id)
 
 # INTRODUCTION ==============================================================
 exp.run_task(task="single_source", phase="intro", direction="forward")
-# Was it loud enough?
-# exp.set_level(90)  # 80dB is the default
-exp.run_task(task="single_source", phase="intro", direction="reversed")
 
 # SINGLE SENTENCE ===========================================================
-exp.run_task(task="single_source", phase="experiment", direction="reversed")
-
-# MULTIPLE SENTENCES - SAME LOCATION ========================================
-exp.run_task(task="multi_source", phase="experiment", plane="colocated")
+exp.run_task(task="single_source", phase="experiment", direction="reversed")  # x2
 
 # MULTIPLE SENTENCES - HORIZONTAL ===========================================
-exp.run_task(task="multi_source", phase="experiment", plane="azimuth")
+exp.run_task(task="multi_source", phase="experiment", plane="azimuth")  # x2
 
 # MULTIPLE SENTENCES - VERTICAL =============================================
-exp.run_task(task="multi_source", phase="experiment", plane="elevation")
+exp.run_task(task="multi_source", phase="experiment", plane="elevation")  # x2
+
+# MULTIPLE SENTENCES - SAME LOCATION ========================================
+exp.run_task(task="multi_source", phase="experiment", plane="colocated")  # x2
 
 
 # RESULTS ===================================================================
 plot_results(subject_id=subject_id, task_type="single_source")
 plot_results(subject_id=subject_id, task_type="multi_source")
-
-# TODO block button press until sound is being played
-# TODO save settings as slab config files
+print_multi_source_linear_model(subject_id=subject_id)
