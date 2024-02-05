2e8a3e7b87ed6b85987e975a396c7e7741ef5dc0
Jakab Pilaszanovich <jakab.pilaszanovich@gmail.com>
2024-02-05 10:44:24 +0100

    Target and masker intelligibility

--- plotting.py
+++ plotting.py
@@ -1,12 +1,12 @@
 import numpy as np
 import pandas as pd
 import seaborn as sns
-import seaborn.objects as so
 import matplotlib.pyplot as plt
 from sklearn import metrics
 import statsmodels.formula.api as smf
 
-# sns.set_theme()
+sns.set_theme()
+palette_tab10 = sns.color_palette("tab10", 10)
 
 df = pd.read_csv("memento_pilot.csv")
 final_subjects = ["pilot_jakab", "pilot_zofia", "pilot_lukas", "pilot_oskar"]
@@ -49,6 +49,19 @@ g.set_xticks(range(len(xticks)), labels=(xticks * 1000).astype(int))
 plt.legend(title='spatial config', loc='upper left')
 plt.savefig(title, dpi=400, overwrite=True)
 
+g = sns.pointplot(
+    data=df_single_source_final,
+    x="target_segment_length",
+    y="score",
+    alpha=0
+)
+title = f"Intelligibility vs segment duration (n={len(df_multi_source_final.subject_id.unique())})"
+g.set_title(title, fontsize=16)
+g.set(xlabel="Reversed segment duration (ms)", ylabel="Intelligibility (%)", ylim=(0, 1.1))
+xticks = np.asarray(sorted(df_single_source_final.target_segment_length.unique()))
+g.set_xticks(range(len(xticks)), labels=(xticks * 1000).astype(int))
+plt.savefig(title, dpi=400)
+
 # Mixed linear model
 md = smf.mixedlm("score ~ masker_segment_length + C(task_plane)", df_multi_source_final, groups="subject_id")
 mdf = md.fit()
@@ -71,4 +84,47 @@ cm_target_display.plot()
 cm_masker_display.plot()
 plt.show()
 
-so.Plot(df_multi_source_final, x="masker_segment_length", color="sex").add(so.Bar(), so.Count(), so.Stack())
+fig, ax = plt.subplots(1, 3, sharey=True)
+masker_segment_lengths = df_multi_source_final.masker_segment_length.unique()
+masker_segment_lengths.sort()
+task_planes = ["collocated", "elevation", "azimuth"]
+N = len(masker_segment_lengths)
+for task_idx, task_plane in enumerate(task_planes):
+    ax_curr = ax[task_idx]
+    df_curr = df_multi_source_final[df_multi_source_final["task_plane"] == task_plane]
+    target_means = df_curr.groupby("masker_segment_length")["score"].mean()
+    masker_means = df_curr.groupby("masker_segment_length")["score_masker"].mean()
+    target_std = df_curr.groupby("masker_segment_length")["score"].std()
+    masker_std = df_curr.groupby("masker_segment_length")["score_masker"].std()
+    ind = np.arange(N)  # the x locations for the groups
+    width = 0.25  # the width of the bars: can also be len(x) sequence
+    p1 = ax_curr.bar(ind - width/2, target_means, width, label="target")
+    p2 = ax_curr.bar(ind + width/2, masker_means, width, label="masker")
+    ax_curr.set_title(task_plane)
+    ax_curr.set_xticks(ind, (masker_segment_lengths * 1000).astype(int))
+    # ax_curr.set_box_aspect(1/0.6)
+lines_labels = fig.axes[0].get_legend_handles_labels()
+fig.legend(lines_labels[0], lines_labels[1], loc='center right')
+fig.supxlabel("Masker reversed segment duration (ms)")
+fig.supylabel('Intelligibility (%)')
+plt.ylim(0, 1.1)
+plt.suptitle('Target - Masker scores')
+plt.show()
+
+
+g = sns.FacetGrid(df_multi_source_final, col="task_plane", sharey=True, sharex=True, col_order=["collocated", "elevation", "azimuth"], height=6, aspect=0.6)
+score_target = g.map(sns.pointplot, "masker_segment_length", "score", color=palette_tab10[0])
+# score_masker = g.map(sns.pointplot, "masker_segment_length", "score_masker", color=palette_tab10[1])
+g.set_titles(template="{col_name}")
+# title = f'Target and Masker intelligibility (n={len(df_multi_source_final.subject_id.unique())})'
+title = f'Target intelligibility in the presence of a masker (n={len(df_multi_source_final.subject_id.unique())})'
+# g.fig.suptitle(title)
+# g.add_legend(labels=["target", "masker"])
+g.add_legend(labels=["target"])
+# g.legend.legendHandles[1].set_color(palette_tab10[1])
+g.set_xlabels(label="Masker reversed segment duration (ms)", clear_inner=True)
+g.set_ylabels(label='Intelligibility (%)')
+xticks = np.asarray(sorted(df_multi_source_final.masker_segment_length.unique()))
+g.set_xticklabels((xticks * 1000).astype(int))
+plt.ylim(0, 1)
+plt.savefig(title, dpi=400)
